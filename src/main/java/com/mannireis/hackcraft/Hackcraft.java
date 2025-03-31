0304bf6b77a328e61334a357ef41c11ccf048208
Manni Reis <90857553+mannireis@users.noreply.github.com>
2025-03-31 17:07:04 +0100

    Added a comment

--- src/main/java/com/mannireis/hackcraft/Hackcraft.java
+++ src/main/java/com/mannireis/hackcraft/Hackcraft.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//Cool Comment
 public class Hackcraft implements ModInitializer {
 	public static final String MOD_ID = "hackcraft";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
