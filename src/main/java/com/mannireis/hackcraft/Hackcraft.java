7e608d2c8fc8de604d74f3a62d6822bd3c0a1d38
Manni Reis <90857553+mannireis@users.noreply.github.com>
2025-03-31 17:05:53 +0100

    Initial commit

---
+++ src/main/java/com/mannireis/hackcraft/Hackcraft.java
@@ -0,0 +1,15 @@
+package com.mannireis.hackcraft;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Hackcraft implements ModInitializer {
+	public static final String MOD_ID = "hackcraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
